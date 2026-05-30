0c02a44d177146658cf6d8ab4173f4f22dbbceba
sahiru <sahiruwrcg@gmail.com>
2026-05-31 02:55:46 +0530

    chore: update allowedDevOrigins IP address in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['192.168.1.10'],
+  allowedDevOrigins: ['192.168.1.5'],
 };
 
 export default nextConfig;
