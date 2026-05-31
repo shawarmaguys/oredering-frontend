5e8283c46ca59a82ad8a19c8d868b1cf4013d86c
sahiru <sahiruwrcg@gmail.com>
2026-05-31 16:34:14 +0530

    chore: add production backend URL to allowedDevOrigins in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['192.168.1.5'],
+  allowedDevOrigins: ['192.168.1.5', 'https://oredering-backend.vercel.app'],
 };
 
 export default nextConfig;
