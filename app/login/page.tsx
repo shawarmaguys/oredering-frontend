f8fa791045c68a7a3c981cfbface45961ee9dd03
sahiru <sahiruwrcg@gmail.com>
2026-06-09 00:23:03 +0530

    feat: implement authentication login page and update dev origin configuration

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -29,7 +29,7 @@ function LoginForm() {
 
     try {
       console.log(process.env.NEXT_PUBLIC_API_URL);
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/v1/auth/login1`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/v1/auth/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
